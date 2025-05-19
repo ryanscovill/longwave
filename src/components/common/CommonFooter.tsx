344e800b8eeef608c8b109e7e58ebe2efe2235a8
ryanscovill <rscovill@sfu.ca>
2025-05-19 11:03:34 -0700

    feat: format header and footer for mobile

--- src/components/common/CommonFooter.tsx
+++ src/components/common/CommonFooter.tsx
@@ -1,41 +1,92 @@
 import { useTranslation } from "react-i18next";
 import { CenteredColumn, CenteredRow } from "./LayoutElements";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { useState, useRef, useEffect } from "react";
+import { glassmorphicStyle } from "./glassmorphicStyle";
 
 export function CommonFooter() {
   const { t } = useTranslation();
+  const [isVisible, setIsVisible] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+        setIsVisible(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <CenteredRow
-      style={{
-        color: "black",
-        fontSize: "small",
-        maxWidth: 700,
-        margin: "auto",
-      }}
-    >
-      <CenteredColumn>
-        <p style={{ margin: 8 }}>
-          <Link href="https://www.wavelength.zone" text="Wavelength" />{" "}
-          {t("commonfooter.developed_by")}{" "}
-          <Link
-            href="https://github.com/cynicaloptimist/longwave"
-            text={t("commonfooter.adapted_for_web")}
-          />{" "}
-          {t("commonfooter.adapted_for_web_by")}
-        </p>
-        {/* we want referrer, so: */}
-        {/* eslint-disable-next-line react/jsx-no-target-blank */}
-        <a target="_blank" href="https://www.patreon.com/improvedinitiative">
-          <img
-            alt="Patreon logo"
-            title={t("commonfooter.support_patreon").toString()}
-            src="./Digital-Patreon-Wordmark_FieryCoral.png"
-            style={{ width: "150px", margin: 8 }}
-          />
-        </a>
-      </CenteredColumn>
-      <AffiliateLink />
-    </CenteredRow>
+    <div ref={containerRef} style={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1000 }}>
+      <div
+        style={{
+          position: "absolute",
+          bottom: 16,
+          left: 16,
+        }}
+      >
+        <div
+          onClick={() => setIsVisible(!isVisible)}
+          style={{
+            cursor: "pointer",
+            padding: 8,
+            ...glassmorphicStyle,
+            borderRadius: "50%",
+            width: 32,
+            height: 32,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+        </div>
+      </div>
+      {isVisible && (
+        <CenteredRow
+          style={{
+            color: "black",
+            fontSize: "small",
+            maxWidth: 700,
+            margin: "auto",
+            padding: 16,
+            ...glassmorphicStyle,
+            borderRadius: 16,
+            marginBottom: 16,
+          }}
+        >
+          <CenteredColumn>
+            <p style={{ margin: 8 }}>
+              <Link href="https://www.wavelength.zone" text="Wavelength" />{" "}
+              {t("commonfooter.developed_by")}{" "}
+              <Link
+                href="https://github.com/cynicaloptimist/longwave"
+                text={t("commonfooter.adapted_for_web")}
+              />{" "}
+              {t("commonfooter.adapted_for_web_by")}
+            </p>
+            {/* we want referrer, so: */}
+            {/* eslint-disable-next-line react/jsx-no-target-blank */}
+            <a target="_blank" href="https://www.patreon.com/improvedinitiative">
+              <img
+                alt="Patreon logo"
+                title={t("commonfooter.support_patreon").toString()}
+                src="./Digital-Patreon-Wordmark_FieryCoral.png"
+                style={{ width: "150px", margin: 8 }}
+              />
+            </a>
+          </CenteredColumn>
+          <AffiliateLink />
+        </CenteredRow>
+      )}
+    </div>
   );
 }
 
